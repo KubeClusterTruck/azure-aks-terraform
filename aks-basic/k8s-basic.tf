967b6ca5738df03c53df60b3e79c35b2a6d790e5
Greg Hoelzer <greg.hoelzer@microsoft.com>
2019-02-12 13:54:24 -0800

    fix location of k8s version in block

--- aks-basic/k8s-basic.tf
+++ aks-basic/k8s-basic.tf
@@ -28,6 +28,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     location            = "${azurerm_resource_group.k8s.location}"
     resource_group_name = "${azurerm_resource_group.k8s.name}"
     dns_prefix          = "${var.dns_prefix}"
+    kubernetes_version  = "${var.aks_version}"
 
     linux_profile {
         admin_username = "ubuntu"
@@ -40,7 +41,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     agent_pool_profile {
         name               = "agentpool"
         count              = "${var.agent_count}"
-        kubernetes_version = "${var.aks_version}"
         vm_size            = "Standard_DS1_v2"
         os_type            = "Linux"
         os_disk_size_gb    = 30
